c836296b66f44630c0642aecf682a4f4bca8b5e1
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-04-06 16:48:51 +0530

    server init and config

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors())
+app.use(morgan("common"))
+
+const PORT = 8000;
+app.listen(PORT);
