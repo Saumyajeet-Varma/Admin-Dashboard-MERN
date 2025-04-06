b873f13aaf81022652fb4466bf5ff8f47e4f5120
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-04-06 16:58:25 +0530

    backend directory structured defined

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,11 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import generalRoutes from './routes/general.routes.js';
+import clientRoutes from './routes/client.routes.js';
+import salesRoutes from './routes/sales.routes.js';
+import managementRoutes from './routes/management.routes.js';
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +20,11 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(cors())
 app.use(morgan("common"))
+
+app.use('/general', generalRoutes);
+app.use('/client', clientRoutes);
+app.use('/sales', salesRoutes);
+app.use('/management', managementRoutes);
 
 const PORT = 8000;
 app.listen(PORT);
